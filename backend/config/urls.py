a221d2c9599c3faf380655360376e0d7ceb21a3b
Vince <202280400@psu.palawan.edu.ph>
2026-08-11 15:27:06 +0800

    Save local url fixes and migrations before merge

--- backend/config/urls.py
+++ backend/config/urls.py
@@ -4,7 +4,7 @@ from django.shortcuts import redirect
 
 urlpatterns = [
     # Redirect root URL to kiosk
-    path('', lambda request: redirect('kiosk/')),
+    path('', lambda request: redirect('/kiosk/')),
 
     # Admin
     path('admin/', admin.site.urls),
@@ -12,9 +12,9 @@ urlpatterns = [
     # Accounts
     path('accounts/', include('accounts.urls')),
 
-    # Customer / Kiosk
-    path('kiosk/', include('customer_portal.urls')),
+    # Customer / Kiosk (Inilagay ang namespace para gumana ang 'customer_portal:kiosk_menu')
+    path('kiosk/', include(('customer_portal.urls', 'customer_portal'), namespace='customer_portal')),
 
-    # Kitchen / Canteen Staff
-    path('kitchen/', include('kitchen_display.urls')),
+    # Kitchen / Canteen Staff (Inilagay din ang namespace para sa kitchen)
+    path('kitchen/', include(('kitchen_display.urls', 'kitchen'), namespace='kitchen')),
 ]
